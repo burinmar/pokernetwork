e8e161053b5f21efd7b040821e6bd301a3a0e293
magas <magas@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-02-10 09:52:59 +0000

    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@308 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- js/modules_adm/other.spotlight.js
+++ js/modules_adm/other.spotlight.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
 		var no = parseInt($('.date-range-div').size());
 		++no;
 
-		var picker1 = '<a href="void(0);" onclick="pickDate(\'active-from-' + no + '\');return false" title=""><img src="'+admImgDir+'_calendar.png" alt="Pick date" width="24" style="margin-bottom:-6px" /></a>&nbsp;';
-		var picker2 = '<a href="void(0);" onclick="pickDate(\'active-to-' + no + '\');return false" title=""><img src="'+admImgDir+'_calendar.png" alt="Pick date" width="24" style="margin-bottom:-6px" /></a>&nbsp;';
+		var picker1 = '<a href="void(0);" onclick="pickDate(\'active-from-' + no + '\');return false" title="" class="calendar"></a>&nbsp;';
+		var picker2 = '<a href="void(0);" onclick="pickDate(\'active-to-' + no + '\');return false" title="" class="calendar"></a>&nbsp;';
 		var html =
 			'<div class="date-range-div">' +
-			' from ' + picker1 + '<input name="active_from[]" id="active-from-' + no + '" value="" class="isDate" size="20" maxlength="20" />' +
-			' till ' + picker2 + '<input name="active_to[]"   id="active-to-' + no + '"   value=""   class="isDate" size="20" maxlength="20" />' +
+			' from ' + picker1 + '<input type="text" name="active_from[]" id="active-from-' + no + '" value="" class="isDate" size="20" maxlength="20" />' +
+			' till ' + picker2 + '<input type="text" name="active_to[]"   id="active-to-' + no + '"   value=""   class="isDate" size="20" maxlength="20" />' +
 			' <a href="#" class="interval-remove" style="color:#DD0000;text-decoration:none;">x</a>' +
 			'</div>';
 
