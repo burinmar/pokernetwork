63042eef72f37f2c9c5ffa872e61a637728ca953
nikita <nikita@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-02-22 12:14:46 +0000

    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@342 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- moon/classes/functions.php
+++ moon/classes/functions.php
@@ -367,4 +367,46 @@ function array_get_del(&$array, $key)
 	return $value;	
 }
 
+function diff($old, $new){
+	$maxlen = 0;
+	foreach($old as $oindex => $ovalue){
+		$nkeys = array_keys($new, $ovalue);
+		foreach($nkeys as $nindex){
+			$matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1])
+				? $matrix[$oindex - 1][$nindex - 1] + 1
+				: 1;
+			if($matrix[$oindex][$nindex] > $maxlen){
+				$maxlen = $matrix[$oindex][$nindex];
+				$omax = $oindex + 1 - $maxlen;
+				$nmax = $nindex + 1 - $maxlen;
+			}
+		}
+	}
+	if($maxlen == 0) {
+		return array(array('d'=>$old, 'i'=>$new));
+	}
+	return array_merge(
+			diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
+			array_slice($new, $nmax, $maxlen),
+			diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen)));
+}
+
+function htmlDiff($old, $new){
+	$old = str_replace("\n", " \n ", $old);
+	$new = str_replace("\n", " \n ", $new);
+	$diff = diff(explode(' ', $old), explode(' ', $new));
+	$ret = '';
+	foreach($diff as $k){
+		if(is_array($k)) {
+			$ret .= (!empty($k['d'])?'<span style="background-color:#FFD8D3; text-decoration: line-through;">'.str_replace("\n","&para;\n",implode(' ',$k['d']))."</span> ":'').
+					(!empty($k['i'])?'<span style="background-color:#CCFFCC;">'.str_replace("\n","&para;\n",implode(' ',$k['i']))."</span> ":'');
+		}
+		else {
+			$ret .= $k . ' ';
+		}
+	}
+	$ret = str_replace(" \n ", "\n", $ret);
+	return $ret;
+}
+
 ?>
