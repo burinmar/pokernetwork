eb0648fa972ac8ac7e1d97f55ffe0f3e5e378102
audrius <audrius@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-03-08 14:55:20 +0000

    Perrašyta shared_img.php
    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@510 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- moon/modules/shared/shared_img.php
+++ moon/modules/shared/shared_img.php
@@ -1,244 +1,184 @@
 <?php
 
-class shared_img{
+class shared_img {
 
-//var $imgDir='';
+	function init() {
+	}
 
-function shared_img()
-{
-}
+	/***** PAGRINDINES FUNKCIJOS *****/
+	// resize image to fit into window $nw x $nh
+	function resize($oFile, $ftarget, $nw, $nh) {
+		list($sw, $sh, $iType) = $this->_checkSize($oFile);
+		$fsource = $oFile->file_path();
+		//resize
+		$sx = $sy = $dx = $dy = 0;
+		$nw = $nw === 'auto' ? $sw:intval($nw);
+		$nh = $nh === 'auto' ? $sh:intval($nh);
+		if ($nw == 0 || $nh == 0) {
+			return FALSE;
+		}
+		$k = min($nw / $sw, $nh / $sh);
+		if ($k > 0.99) {
+			//resaizinti nereikia
+			return $this->_justCopy($fsource, $ftarget);
+		}
+		$dh = round($k * $sh);
+		$dw = round($k * $sw);
+		return $this->_transform($oFile, $ftarget, $dx, $dy, $sx, $sy, $dw, $dh, $sw, $sh);
+	}
 
-function init()
-{
-	//$this->imgDir='';
-}
+	function crop($oFile, $ftarget, $nw, $nh = 'auto', $left = 'auto', $top = 'auto') {
+		list($sw, $sh, $iType) = $this->_checkSize($oFile);
+		$fsource = $oFile->file_path();
+		//crop
+		$sx = $sy = $dx = $dy = 0;
+		$nw = $nw === 'auto' ? $sw:intval($nw);
+		$nh = $nh === 'auto' ? $sh:intval($nh);
+		if ($nw == 0 || $nh == 0 || $sw == 0 || $sh == 0) {
+			return FALSE;
+		}
+		if ($left === 'auto') {
+			$left = floor(($sw - $nw) / 2);
+		}
+		if ($top === 'auto') {
+			$top = floor(($sh - $nh) / 2);
+		}
+		$sx = $left;
+		$sy = $top;
+		$dh = $sh = $nh;
+		$dw = $sw = $nw;
+		return $this->_transform($oFile, $ftarget, $dx, $dy, $sx, $sy, $dw, $dh, $sw, $sh);
+	}
 
-//***** PAGRINDINES FUNKCIJOS *****
-function resize($fileObj,$ftarget,$nw,$nh)
-{ //resaizina paveiksliuka. Jei pavyko, grazina true, kitu atveju false
-	$palaiko=array('jpg'=>IMG_JPG, 'png'=>IMG_PNG, 'gif'=>IMG_GIF);
-	$types=array('jpg'=>IMAGETYPE_JPEG, 'png'=>IMAGETYPE_PNG, 'gif'=>IMAGETYPE_GIF);
-	$ext=$fileObj->file_ext();
-	if ($fileObj->file_ext($ftarget)!==$ext) return false;
-	if ($ext==='jpeg') $ext='jpg';
-    
-    $fsource = $fileObj->file_path();
-    $img_info = getimagesize($fsource);
+	function thumbnail($oFile, $saveAs, $width) {
+		return $this->resize_exact($oFile, $saveAs, $width, $width);
+	}
 
-	if (!isset($palaiko[$ext]) || !($tipai=imagetypes() & $palaiko[$ext])) return false;
-    if(!is_array($img_info) || $img_info[2]!=$types[$ext]) return false;
-	
-	list($x,$y)=explode('x',$fileObj->file_wh());
-	$nw = $nw==='auto' ? $x : intval($nw);
-	$nh = $nh==='auto' ? $y : intval($nh);
-	if ($x*$y==0 || $nw*$nh==0) return false;
-	$k=min($nw/$x , $nh/$y);//didinimo koeficientas
-	if ($k>0.99) { //resaizinti nereikia
-		if ($ftarget==$fsource) return $fileObj;
-		if (file_exists($ftarget)) unlink($ftarget);
-		copy($fsource,$ftarget);
-		chmod($ftarget,0666);
+	// resize image to be exactly $nw x $nh
+	function resize_exact($oFile, $ftarget, $nw, $nh) {
+		list($sw, $sh, $iType) = $this->_checkSize($oFile);
+		$fsource = $oFile->file_path();
+		//resize exact
+		if ($nw == 0 || $nh == 0) {
+			return FALSE;
+		}
+		$sx = $sy = $dx = $dy = 0;
+		$k = min($sw / $nw, $sh / $nh);
+		if ($k < 1) {
+			//resaizinti nereikia
+			//return $this->_justCopy($fsource, $ftarget);
+		}
+		$h = round($k * $nh);
+		$w = round($k * $nw);
+		if ($h < $sh) {
+			//crop virsu
+			$sy = floor(($sh - $h) / 2);
+			$sh -= 2 * $sy;
+		}
+		else {
+			$sx = floor(($sw - $w) / 2);
+			$sw -= 2 * $sx;
+		}
+		$dh = $nh;
+		$dw = $nw;
+		return $this->_transform($oFile, $ftarget, $dx, $dy, $sx, $sy, $dw, $dh, $sw, $sh);
+	}
+
+	function _checkSize($oFile) {
+		$fsource = $oFile->file_path();
+		$i = file_exists($fsource) ? getimagesize($fsource):FALSE;
+		if ($i === FALSE || empty ($i[0]) || empty ($i[1]) || empty ($i[2]) || !($i[2] & imagetypes())) {
+			//tai ne paveiksliukas, arba nepalaikomas tipas
+			return array(0, 0, 0);
+		}
+		return $i;
+	}
+
+	function _justCopy($fsource, $ftarget) {
+		//resaizinti nereikia
+		if ($ftarget == $fsource) {
+			return TRUE;
+		}
+		if (file_exists($ftarget)) {
+			unlink($ftarget);
+		}
+		if (copy($fsource, $ftarget)) {
+			$oldumask = umask(0);
+			chmod($ftarget, 0666);
+			umask($oldumask);
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	function _transform($oFile, $ftarget, $dx, $dy, $sx, $sy, $dw, $dh, $sw, $sh) {
+		$fsource = $oFile->file_path();
+		list(,, $iType) = getimagesize($oFile->file_path());
+		if (str_replace('jpg', 'jpeg', $oFile->file_ext($ftarget)) !== image_type_to_extension($iType, FALSE)) {
+			// target pletinys neatitinka imgtype
+			return FALSE;
+		}
+		switch ($iType) {
+
+			case IMAGETYPE_JPEG:
+				$img = imagecreatefromjpeg($fsource);
+				break;
+
+			case IMAGETYPE_PNG:
+				$img = imagecreatefrompng($fsource);
+				break;
+
+			case IMAGETYPE_GIF:
+				$img = imagecreatefromgif($fsource);
+				break;
+
+			default:
+				return FALSE;
+		}
+		$newImg = imagecreatetruecolor($dw, $dh);
+		//transparentumas
+		if ($iType == IMAGETYPE_GIF || $iType == IMAGETYPE_PNG) {
+			$trnprtIndx = imagecolortransparent($img);
+			if ($trnprtIndx >= 0) {
+				$trnprtColor = imagecolorsforindex($img, $trnprtIndx);
+				$trnprtIndx = imagecolorallocate($newImg, $trnprtColor['red'], $trnprtColor['green'], $trnprtColor['blue']);
+				imagefill($newImg, 0, 0, $trnprtIndx);
+				imagecolortransparent($newImg, $trnprtIndx);
+			}
+			elseif ($iType == IMAGETYPE_PNG) {
+				imagealphablending($newImg, false);
+				imagesavealpha($newImg, true);
+				$transparent = imagecolorallocatealpha($newImg, 255, 255, 255, 127);
+				imagefilledrectangle($newImg, 0, 0, $dw, $dh, $transparent);
+			}
+		}
+		//end transparentumas
+		imagecopyresampled($newImg, $img, $dx, $dy, $sx, $sy, $dw, $dh, $sw, $sh);
+		if (file_exists($ftarget)) {
+			unlink($ftarget);
+		}
+		switch ($iType) {
+
+			case IMAGETYPE_JPEG:
+				imagejpeg($newImg, $ftarget, 90);
+				break;
+
+			case IMAGETYPE_PNG:
+				imagepng($newImg, $ftarget);
+				break;
+
+			case IMAGETYPE_GIF:
+				imagegif($newImg, $ftarget);
+				break;
+		}
+		imagedestroy($newImg);
+		imagedestroy($img);
+		$oldumask = umask(0);
+		chmod($ftarget, 0666);
+		umask($oldumask);
 		return true;
 	}
-	$h=round($k*$y);
-	$w=round($k*$x);
-
-	switch ($ext) {
-    case 'jpg': $img=imagecreatefromjpeg($fsource); break;
-	case 'png': $img=imagecreatefrompng($fsource); break;
-	case 'gif': $img=imagecreatefromgif($fsource); break;
-	default: return false;
-	}
-	$mini = imagecreatetruecolor($w,$h);
-	imagecopyresampled($mini,$img,0,0,0,0,$w,$h,$x,$y);
-	if (file_exists($ftarget)) unlink($ftarget);
-    switch ($ext) {
-    case 'jpg': imagejpeg($mini,$ftarget,90); break;
-	case 'png': imagepng($mini,$ftarget); break;
-	case 'gif': imagegif($mini,$ftarget); break;
-	}
-	imagedestroy($mini);
-	imagedestroy($img);
-    chmod($ftarget,0666);
-	return true;
-}
-
-function crop($fileObj, $ftarget, $nw, $nh='auto', $left='auto', $top='auto')
-{
-    $palaiko=array('jpg'=>IMG_JPG, 'png'=>IMG_PNG, 'gif'=>IMG_GIF);
-	$ext=$fileObj->file_ext();
-	if ($fileObj->file_ext($ftarget)!==$ext) return false;
-	if ($ext==='jpeg') $ext='jpg';
-	if (!isset($palaiko[$ext]) || !($tipai=imagetypes() & $palaiko[$ext])) return false;
-	$fsource=$fileObj->file_path();
-	list($x,$y)=explode('x',$fileObj->file_wh());
-	$nw = $nw==='auto' ? $x : intval($nw);
-	$nh = $nh==='auto' ? $y : intval($nh);
-	if ($x*$y==0 || $nw*$nh==0) return false;
-
-    if ($left==='auto') $left=floor(($x-$nw)/2);
-	if ($top==='auto') $top=floor(($y-$nh)/2);
-
-    switch($ext) {
-        case 'gif':  $img = imagecreatefromgif($fsource);    break;
-        case 'jpg':  $img = imagecreatefromjpeg($fsource);   break;
-        case 'png':  $img = imagecreatefrompng($fsource);    break;
-		default: return false;
-    }
-    $dimg = imagecreatetruecolor($nw, $nh);
-    imagecopyresampled($dimg,$img,0,0,$left,$top,$nw,$nh,$nw,$nh);
-    //$sharpenMatrix = array(-1,-1,-1,-1,16,-1,-1,-1,-1);
-	//$divisor = 8;
-	//$offset = 0;
-	//imageconvolution($dimg, $sharpenMatrix, $divisor, $offset);
-    if (file_exists($ftarget)) unlink($ftarget);
-    switch (strtolower($ext)) {
-		case 'jpg': imagejpeg($dimg, $ftarget, 90); break;
-		case 'png': imagepng($dimg, $ftarget); break;
-		case 'gif': imagegif($dimg, $ftarget); break;
-	}
-    imagedestroy($img);
-	imagedestroy($dimg);
-    chmod($ftarget,0666);
-	return true;
-}
-
-function thumbnail(&$fileObj,$saveAs,$width)
-{
-    $name=basename($saveAs);
-	$tmpFile=dirname($saveAs).'/'.'crop_'.$name;
-	list($x,$y)=explode('x',$fileObj->file_wh());
-	$nw=min($x,$y);
-	if ($this->crop($fileObj,$tmpFile,$nw,$nw,'auto','auto') && $fileObj->is_file($tmpFile)){
-	   	$ok=$this->resize($fileObj,$saveAs,$width,$width);
-	   	unlink($tmpFile);
-		return $ok;
-	}
-    return false;
-}
-
-function resize_exact(&$fileObj, $ftarget, $nw, $nh) {
-    $palaiko=array('jpg'=>IMG_JPG, 'png'=>IMG_PNG, 'gif'=>IMG_GIF);
-	$ext=$fileObj->file_ext();
-	if ($fileObj->file_ext($ftarget)!==$ext) return false;
-	if ($ext==='jpeg') $ext='jpg';
-	if (!isset($palaiko[$ext]) || !($tipai=imagetypes() & $palaiko[$ext])) return false;
-	$fsource=$fileObj->file_path();
-	list($w,$h)=explode('x',$fileObj->file_wh());
-
-    switch(strtolower($ext)) {
-        case 'gif':  $img = imagecreatefromgif($fsource);    break;
-        case 'jpg':  $img = imagecreatefromjpeg($fsource);   break;
-        case 'png':  $img = imagecreatefrompng($fsource);    break;
-    }
-
-    $curr_w = $w;
-    $curr_h = $h;
-
-    $w = $nw;
-    $h = $nh;
-    
-	// if new width and height are smaller than current - do not resize
-	if (($curr_w <= $w) AND ($curr_h <= $h)) {
-		if ($ftarget==$fsource) return $fileObj;
-		if (file_exists($ftarget)) unlink($ftarget);
-		copy($fsource,$ftarget);
-		chmod($ftarget,0666);
-		return true;
-	}
-
-    if($w && ($curr_w<$curr_h)) {
-        $w = ($h / $curr_h) * $curr_w;
-    }
-    else {
-        $h = ($w / $curr_w) * $curr_h;
-    }
-
-    if($w<$nw) {
-        $c = ($nw / $w);
-        $w = $c * $w;
-        $h = $c * $h;
-    }
-    if($h<$nh) {
-        $c = ($nh / $h);
-        $w = $c * $w;
-        $h = $c * $h;
-    }
-
-    $data = array();
-    $data['src_x'] = 0;
-    $data['src_y'] = 0;
-    $data['src_w'] = $curr_w;
-    $data['src_h'] = $curr_h;
-
-    $data['dst_x'] = 0;
-    $data['dst_y'] = 0;
-    $data['dst_w'] = $w;
-    $data['dst_h'] = $h;
-
-    $gd_resource_tmp = imagecreatetruecolor($data['dst_w'], $data['dst_h']);
-
-    /*
-    $image_resized = &$gd_resource_tmp;
-    $image = &$img;
-
-    if (strtolower($ext) == 'gif' || strtolower($ext) == 'png') {
-      $trnprt_indx = imagecolortransparent($image);
-      if ($trnprt_indx >= 0) {
-        $trnprt_color    = imagecolorsforindex($image, $trnprt_indx);
-        $trnprt_indx    = imagecolorallocate($image_resized, $trnprt_color['red'], $trnprt_color['green'], $trnprt_color['blue']);
-        imagefill($image_resized, 0, 0, $trnprt_indx);
-        imagecolortransparent($image_resized, $trnprt_indx);
-      }
-      elseif (strtolower($ext) == 'png') {
-        imagealphablending($image_resized, false);
-        $color = imagecolorallocatealpha($image_resized, 0, 0, 0, 127);
-        imagefill($image_resized, 0, 0, $color);
-        imagesavealpha($image_resized, true);
-      }
-    }
-
-    $gd_resource_tmp = &$image_resized;
-
-    foreach ($data as $k=>$v) {
-    	$data[$k] = round($v);
-    }
-    imagecopyresampled($gd_resource_tmp, $img, $data['dst_x'], $data['dst_y'], $data['src_x'], $data['src_y'], $data['dst_w'], $data['dst_h'], $data['src_w'], $data['src_h']);
-    $dimg = &$gd_resource_tmp;
-    */
-
-    imagecopyresampled($gd_resource_tmp, $img, $data['dst_x'], $data['dst_y'], $data['src_x'], $data['src_y'], $data['dst_w'], $data['dst_h'], $data['src_w'], $data['src_h']);
-    $dimg = $gd_resource_tmp;
-
-    $w = $nw;
-    $h = $nh;
-
-    $curr_w = imagesx($dimg);
-    $curr_h = imagesy($dimg);
-
-    $data['src_x'] = floor(($curr_w - $w + 1) / 2);
-    $data['src_y'] = floor(($curr_h - $h + 1) / 2);
-    $data['src_w'] = $data['dst_w'] = $w;
-    $data['src_h'] = $data['dst_h'] = $h;
-
-    $data['dst_x'] = 0;
-    $data['dst_y'] = 0;
-
-    $gd_resource_tmp = imagecreatetruecolor($data['dst_w'], $data['dst_h']);
-    imagecopyresampled($gd_resource_tmp, $dimg, $data['dst_x'], $data['dst_y'], $data['src_x'], $data['src_y'], $data['dst_w'], $data['dst_h'], $data['src_w'], $data['src_h']);
-    $dimg = $gd_resource_tmp;
-
-    if (file_exists($ftarget)) unlink($ftarget);
-    switch (strtolower($ext)) {
-		case 'jpg': imagejpeg($dimg, $ftarget, 90); break;
-		case 'png': imagepng($dimg, $ftarget); break;
-		case 'gif': imagegif($dimg, $ftarget); break;
-	}
-    imagedestroy($img);
-	imagedestroy($dimg);
-    chmod($ftarget,0666);
-	return true;
-}
 
 }
+
 ?>
