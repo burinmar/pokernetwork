01d25ad459c27ea5cc333f62bd874e1e5a850b1c
nikita <nikita@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-03-09 15:52:28 +0000

    sys.column (reporting)
    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@518 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- moon/modules/sys/column.php
+++ moon/modules/sys/column.php
@@ -1,43 +1,27 @@
 <?php
 
+class column extends moon_com 
+{
+	function main($vars) 
+	{
+		$tpl  = $this->load_template();
+		$page = moon::page();
+		$tplArgv = array(
+			'banner' => '',
+		);
+		// $navi = moon::shared('sitemap');
+		// $breadcrumb = $navi->breadcrumb();
+		// // $pID = $navi->on();
+		// $isIndexPage = ($page->uri_segments(1) === '');
 
-class column extends moon_com {
-
-
-	function main($vars) {
-		return $this->load_template()->parse('main');
-
-		$tpl = & $this->load_template();
-		$navi = & moon :: shared('sitemap');
-		$bCrumb = $navi->breadcrumb();
-		$activeMainMenu = isset ($bCrumb[1]['id']) ? $bCrumb[1]['id'] : 0;
-		// main menu
-		$mainMenu = $navi->listLevel(2);
-		if (!count($mainMenu)) {
-			return '';
-		}
-		$m = array('menu' => '');
-		foreach ($mainMenu as $id => $d) {
-			$class = '';
-			if ($d['class']) {
-				$class = $d['class'];
-			}
-			if ($activeMainMenu && $activeMainMenu == $id) {
-				$class = ltrim($class . ' active');
-			}
-			$d['td:class'] = $class ? ' class="' . $class . '"' : '';
-			$d['title'] = htmlspecialchars($d['title']);
-			$m['menu'] .= $tpl->parse('menu', $d);
-		}
-		$m['url.search'] = $navi->getLink('search');
-		$m['isHome'] =  'home' == $navi->on();
-
-		//$page->js('/js/pnslider.js');
-		$m['url.search'] = $this->link('search');
-		return $tpl->parse('main', $m);
+		// banneris
+		// if (!$isIndexPage) {
+		// 	// rooms sarase eina i gala
+		// 	$pos = !empty($breadcrumb[0]['page_id']) && $breadcrumb[0]['page_id'] == 'rooms' 
+		// 		? '2' 
+		// 		: '';
+		// 	$tplArgv['banner' . $pos] = $tpl->parse('banner');
+		// }
+		return $tpl->parse('main',$tplArgv);
 	}
-
-
 }
-
-?>
