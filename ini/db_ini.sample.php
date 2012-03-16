138b974df951c8ebb56c6674f1e15b6cd63aaad8
audrius <audrius@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-03-16 09:09:00 +0000

    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@543 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- ini/db_ini.sample.php
+++ ini/db_ini.sample.php
@@ -8,5 +8,15 @@ database=pokernetwork
 charset=utf8
 page503=503.htm
 
+
+[database-vb]
+dbclass=mysql4
+server=192.168.0.11:3307
+user=pnteamvb
+password=8yWYJa
+database=pokernetwork_forum
+charset=utf8
+
+
 [site]
 home_url=http://www.pokernetwork.dev/
---
+++ moon/modules/users/vb.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class vb extends moon_com {
+
+
+	function onload() {
+		/* table Users*/
+		$this->myTable = 'vb_user';
+		$this->db = & moon::db('database-vb');
+	}
+
+
+	function users($ids) {
+		if (!is_array($ids)) {
+			$ids = array($ids);
+		}
+		foreach ($ids as $k=>$v) {
+			$ids[$k]=intval($v);
+		}
+		$ids = array_unique($ids);
+		if (!count($ids)) {
+			return array();
+		}
+		$sql='SELECT userid as id, username as nick FROM '.$this->myTable.' WHERE userid IN ('.implode(',',$ids).')';
+		$n=$this->db->array_query_assoc(  $sql , 'id');
+		foreach ($n as $id=>$v) {
+			$n[$id]['avatar'] = '';
+		}
+		return $n;
+	}
+
+}
+
+?>
