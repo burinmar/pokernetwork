1ebc480ae275a20ac551fb7516463ddf88667d06
magas <magas@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-03-26 12:26:15 +0000

    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@610 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- moon/modules/sys/tmp.php
+++ moon/modules/sys/tmp.php
@@ -7,8 +7,7 @@ class tmp extends moon_com {
 		}
 		
 		$page = & moon :: page();
-		$page->css('/css/article.css');
-		$page->css('/css/landing.css');
+		$page->css('/css/hof.css');
 		//$page->set_local('nobanners', 1);
 		
 
