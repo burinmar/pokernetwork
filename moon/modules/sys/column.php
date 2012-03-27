0c0f89ed703278fc0505c4911a40688a93221418
audrius <audrius@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-03-27 11:32:53 +0000

    PNW-107 PokerNetwork Learn Poker skiltis (meniu)
    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@615 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- moon/modules/sys/column.php
+++ moon/modules/sys/column.php
@@ -23,6 +23,20 @@ class column extends moon_com
 		// 	$tplArgv['banner' . $pos] = $tpl->parse('banner');
 		// }
 
+		$navi = moon::shared('sitemap');
+		$breadcrumb = $navi->breadcrumb();
+		$pID = isset ($breadcrumb[0]['page_id']) ? $breadcrumb[0]['page_id'] : 0;
+		$active = isset ($breadcrumb[1]['id']) ? $breadcrumb[1]['id'] : 0;
+		if ('rules' == $pID) {
+			$tplArgv['menu'] = '';
+			$menu = $navi->listLevel(2);
+			foreach ($menu as $id => $d) {
+				$d['classActive'] = $active == $id ? ' class="active"' : '';
+				$d['title'] = htmlspecialchars($d['title']);
+				$tplArgv['menu'] .= $tpl->parse('menu', $d);
+			}
+		}
+
 		$oRooms = $this->object('reviews.rooms_box');
 		$tplArgv['rooms-box'] = $oRooms->main(array());
 
