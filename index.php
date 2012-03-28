5d0fa1161de5179aa4b710bab98a50ddba99f3e2
audrius <audrius@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-03-28 12:15:52 +0000

    apsaugotas passwordu
    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@627 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- index.php
+++ index.php
@@ -5,23 +5,16 @@ if (is_dev()) {
 	ini_set('display_errors', TRUE);
 	ini_set('error_reporting', E_ALL);
 }
-/*elseif (isset ($_SERVER['HTTP_HOST'])) {
+elseif (isset ($_SERVER['HTTP_HOST'])) {
 	//	DEMO SECURITY
-	$sites = array('bg','nl','de','gr','hu','it','ro','si');
-	foreach ($sites as $id) {
-		if (strpos($_SERVER['HTTP_HOST'], $id.'.casinogrinder')!==FALSE) {
-
-			if(isset($_POST['demopass']) && $_POST['demopass']=='welcome-' . $id){
-				setcookie('demoin',1,time()+3600*6,'/');
-			} else if((!isset($_COOKIE['demoin']) || $_COOKIE['demoin']!=1) && (!isset($_COOKIE['voter2']) || $_COOKIE['voter2']!=='Transporter-Pokernews')){
-		        include('soon.htm');
-				die();
-			}
-		}
+	if(isset($_POST['demopass']) && $_POST['demopass']=='welcome'){
+		setcookie('demoin',1,time()+3600*6,'/');
+	} else if((!isset($_COOKIE['demoin']) || $_COOKIE['demoin']!=1) && (!isset($_COOKIE['voter2']) || $_COOKIE['voter2']!=='Transporter-Pokernews')){?>
+		<html><body><form method="POST">Enter password: <input type="password" name="demopass"> <input type="submit" value="Login"></form></body></html>
+		<?	die();
 	}
-
 	//DEMO SECURITY
-}*/
+}
 moon_init('ini/moon.ini', 'engine');
 moon_reconfig();
 $all = moon_process();
