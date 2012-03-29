3bf1bffb2252f303141d54c6435099c829484579
audrius <audrius@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-03-29 07:05:48 +0000

    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@632 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- moon/modules/users/config.cfg.php
+++ moon/modules/users/config.cfg.php
@@ -1,21 +1,8 @@
 <?php
 $cfg = array();
 $cfg['users'] = array(
-	'vocabulary' => '{dir.multilang}{module}.txt;{dir.multilang}shared.txt',
 	'tb.Users' => 'users',
-	'tb.UsersRooms' => 'users_rooms',
-	'tb.UsersAccess' => 'users_levels',
-	'tb.UsersCriminals' => 'users_criminals',
-	'tb.Subscribers' => 'subscribers', 
-	'tb.Rooms' => 'rw2_rooms',
-
-    'page.Signup' => 'xml.1col,fake',
-    'page.Profile' => 'xml.1col,fake',
-    'page.Users' => 'xml.1col,fake',
-
+	'page.Signup' => 'xml.1col,fake',
 	'comp.login_object' => 'MoonShared.login_object',
-
-	//'var.srcAvatars' => '/w/avatars/',
-	//'var.srcRoomsLogo' => '/w/rw-logo/'
 );
 ?>
