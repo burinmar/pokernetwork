56a84d83456df5f456a8cdd78651c8e342815b2f
audrius <audrius@dad3e664-c0dc-499b-bd67-bed0936aec3c>
2012-06-14 11:40:22 +0000

    PN-3089 Pagerinti wallpaper valdymą
    git-svn-id: http://svn.ibus.lt/pnteam/trunk/pokernetwork@996 dad3e664-c0dc-499b-bd67-bed0936aec3c

--- moon/modules/sys/header.php
+++ moon/modules/sys/header.php
@@ -28,62 +28,39 @@ class header extends moon_com
 
 		$res = $tpl->parse('main', $tplArgv);
 
+		// *** WALLPAPERS ***
 		$isHomepage = 'home' == $navi->on();
 		if (!$isHomepage) {
 			return $res;
 		}
-		
 
-		$bgURL = '/leagues/30k-winner-wednesday-dozen/';
-		$res = '<script type="text/javascript">var bgURL = "'.$bgURL.'";</script>' . $res;
-		$page->css('<style type="text/css">/*<![CDATA[*/ html { background: #1e0000 url(\'/img/winner_30k_wallpaper.jpg\') no-repeat fixed top center; cursor: pointer;} /*]]>*/</style>');
-
-		return $res;
-
-		$bgURL = 'http://www.pokernetwork.com/leagues/10-500-poker770-weekend-wonders/';
-		$res = '<script type="text/javascript">var bgURL = "'.$bgURL.'";</script>' . $res;
-		$page->css('<style type="text/css">/*<![CDATA[*/ html { background: #000000 url(\'/img/p770_wonders_wallpaper.jpg\') no-repeat fixed top center; cursor: pointer;} /*]]>*/</style>');
-		return $res;
-		/* Toliau baneris */
-		$cookieVar = 'b0521';
-		if (!isset($_COOKIE[$cookieVar]) || $_COOKIE[$cookieVar] !='h') {
-			if(isset($_GET['bps']) && $_GET['bps'] == 'hide') {
-				setcookie($cookieVar, 'h', time()+86400);
-			}
-			$res = '<script type="text/javascript">function closeFlashLayer(){var d=new Date();d.setTime(Date.UTC(d.getFullYear(),d.getMonth(),d.getDate(),23,59,59,999)+d.getTimezoneOffset()*60000+1);document.cookie=\''.$cookieVar.'=h; expires=\'+d.toGMTString()+\'; path=/\';document.getElementById(\'bn0902\').style.display=\'none\'}</script><div style="position: fixed; top: 0; right: 50%; margin-right: -508px; width: 1012px; height: 611px; z-index: 9999;" id="bn0902" onclick="closeFlashLayer()"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="1012" height="611" id="mymoviename""><param name="movie" value="/img/p770_wonders_layer_banner.swf?clickTAG='.$bgURL.'" /><param name="quality" value="high" /><param name="allowscriptaccess" value="always"><param name="quality" value="high" /><param name="wmode" value="transparent" /><param name="bgcolor" value="transparent" /><embed src="/img/p770_wonders_layer_banner.swf?clickTAG='.$bgURL.'" wmode="transparent" quality="high" bgcolor="transparent" width="1012" height="611" name="mymoviename" allowscriptaccess="always" align="" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></div>' . $res;
-		}
-		
+if ($this->wallpaper($res, array(
+'url'=> '/leagues/30k-winner-wednesday-dozen/',
+'imgPath'=> '/img/winner_30k_wallpaper.jpg',
+'bgColor'=> '#1e0000',
+)
+)) return $res;
 
 
-		
-		
-		$bgURL = '/leagues/pkr-daily-dollar-wsop-rake-chase/';
-		$res = '<script type="text/javascript">var bgURL = "'.$bgURL.'";</script>' . $res;
-		$page->css('<style type="text/css">/*<![CDATA[*/ html { background: #000 url(\'/img/pkr_wsop_wallpaper.jpg\') no-repeat fixed top center; cursor: pointer;} /*]]>*/</style>');
 
 
 		/************* SLAMSTAS *************/
-
+        /*
 		// PokerStars SCOOP 2012
 		//banner
 		$psb =$tpl->parse('pokerstars', array('siteID'=>'EN'));
 		$tb = '<div style="background: #000; margin: 0 auto; text-align: center; width: 1000px;">' .$psb. '</div>';
-		//moon::page()->set_local('topbanner', $tb);
 		//background
-		$bgURL = 'http://pokerstars.com/EN/ad/11216421/1000x150zoomtakeover.gif.click?rq=noscript&vs=';
-		$res = '<script type="text/javascript">var bgURL = "'.$bgURL.'";</script>' . $tb . $res;
-		$page->css('<style type="text/css">/*<!CDATA*/ body {background: #000 url(\'/img/zoom_wallpaper_2000x1200.jpg\') no-repeat fixed top} html {background: #000; cursor: pointer} #bodyBlock, #footerBlock {cursor: default} /*>*/</style>');
-		return $res;
+
 
 		//wallpaper
 		//banner
 		$psb =$tpl->parse('pokerstars', array('siteID'=>'EN'));
 		$tb = '<div style="background: #000; margin: 0 auto; text-align: center; width: 1000px;">' .$psb. '</div>';
 		//moon::page()->set_local('topbanner', $tb);
-		//background
+        //background
 		$bgURL = 'http://pokerstars.com/EN/ad/11216421/1000x150scoop.gif.click?rq=noscript&vs=';
-		$res = '<script type="text/javascript">var bgURL = "'.$bgURL.'";</script>' . $tb . $res;
-		$page->css('<style type="text/css">/*<!CDATA*/ body {background: #000 url(\'/img/ps_scoop2012_wallpaper.jpg\') no-repeat fixed top} html {background: #000; cursor: pointer} #bodyBlock, #footerBlock {cursor: default} /*>*/</style>');
+		*/
 
 
 	}
@@ -192,4 +169,125 @@ class header extends moon_com
 	{
 		return !empty ($item['hide']) || !isset ($item['url']);
 	}
+
+    /*
+if ($this->wallpaper($res, array(
+'roomID'=> 53,
+//paprastas urlas. Jei prasideda +, tai kartu su roomID naudojamas ( '+ext/')
+// +freerolls/1365.htm?master
+// +freerolls/?upcoming
+// +download/
+'url'=> 'http://www.site.com/xx.htm',
+'imgPath'=> '/i/xx.jpg',
+'bgColor'=> '#000',
+'geoTarget'=> 'us, in, bg',
+'startDate'=> '2012-06-08 23:59:59',
+'endDate'=> '2012-06-10 23:59:59',
+'showIn'=> 'home, rules, reporting',
+//galima gal netgi apsiraðyti  flash layer banner?
+'layerPath'=> '/i/xxx.swf',
+'layerSize'=> '1024x100',
+'layerDivCSS'=> 'position: absolute, top: 0',
+)
+)) return $res;
+*/
+
+
+function wallpaper(&$res, $w) {
+	//
+	if (!empty($w['startDate'])) {
+		$from = strtotime($w['startDate']);
+		if ($from === FALSE || $from > moon::locale()->now()) {
+			$delta = $from - moon::locale()->now();
+			$res .= '<!-- delta '.(floor($delta/3600) .':'.floor(($delta % 3600)/60)).':'.floor($delta % 60).' -->';
+			return FALSE;
+		}
+	}
+	if (!empty($w['endDate'])) {
+		$to = strtotime($w['endDate']);
+		if ($to === FALSE || $to < moon::locale()->now()) {
+			return FALSE;
+		}
+	}
+
+	//
+	if (empty($w['geoTarget'])) {
+		$w['geoTarget'] = '-us,gb,in,gi';
+	}
+	$minus = $w['geoTarget']{0} === '-';
+	if ($minus && $w['geoTarget'] == '-') {
+		$w['geoTarget'] .= '*';
+	}
+	$sites = explode(',', str_replace(array(' ','-'), '', $w['geoTarget']));
+	$country = geo_my_country();
+	if ((!$minus && !in_array($country, $sites)) || ($minus && in_array($country, $sites))) {
+		return FALSE;
+	}
+	//
+	$navi = moon::shared('sitemap');
+	if (!empty($w['showIn'])) {
+		$on = $navi->on();
+		$minus = $w['showIn']{0} === '-';
+		$sites = explode(',', str_replace(array(' ','-'), '', $w['showIn']));
+		if ((!$minus && !in_array($on, $sites)) || ($minus && in_array($on, $sites))) {
+			return FALSE;
+		}
+	}
+	$bgURL = FALSE;
+	if (!empty($w['roomID'])) {
+		$is = $this->db->single_query('SELECT alias FROM rw2_rooms WHERE id=' . (int)$w['roomID'] . ' AND is_hidden=0');
+		if (!empty($is[0])) {
+			$bgURL = '/' . $is[0] . '/';
+			if (!empty($w['url']) && $w['url']{0} ==='+') {
+				$bgURL .= ltrim($w['url'], '+');
+			}
+		}
+		else {
+			return FALSE;
+		}
+	}
+	if (!empty($w['promoID'])) {
+		if ($promoLink = $navi->getLink('promotions')) {
+			$field = 'com' === _SITE_ID_ ? 'id' : 'remote_id';
+			$is = $this->db->single_query('SELECT alias FROM promos WHERE ' . $field . ' = 107 AND is_hidden = 0');
+		}
+		if ($promoLink && !empty($is[0])) {
+			$bgURL = $promoLink . $is[0] . '/';
+		}
+		else {
+			return FALSE;
+		}
+	}
+	elseif (!empty($w['url']) && $w['url']{0} !=='+') {
+		$bgURL = $w['url'];
+	}
+
+	if ($bgURL !== FALSE) {
+		$bgColor = empty($w['bgColor']) ? '#000000' : $w['bgColor'];
+		if (!empty($w['imgPath'])) {
+			$res .= '<script type="text/javascript">var bgURL = "'.$bgURL.'";</script>';
+			moon::page()->css('<style type="text/css">/*<![CDATA[*/ body {background: '.$bgColor.' url(\''.$w['imgPath'].'\') no-repeat fixed top} html {background: '.$bgColor.'; cursor: pointer} #bodyBlock, #footerBlock {cursor: default} /*]]>*/</style>');
+		}
+
+		/* Toliau baneris */
+		if (!empty($w['layerPath'])) {
+			$cookieVar = 'b'.substr(md5($w['layerPath']),-10);
+			if (!isset($_COOKIE[$cookieVar]) || $_COOKIE[$cookieVar] !='h') {
+				if(isset($_GET['bps']) && $_GET['bps'] == 'hide') {
+					setcookie($cookieVar, 'h', moon::locale()->now() + 86400);
+				}
+				$width = $height = 0;
+				if (!empty($w['layerPath'])) {
+					list($width, $height) = explode('x', $w['layerSize'] . 'x0');
+				}
+				$css = empty($w['layerDivCSS']) ? '' : $w['layerDivCSS'];
+				$res = '<script type="text/javascript">function closeFlashLayer(){var d=new Date();d.setTime(Date.UTC(d.getFullYear(),d.getMonth(),d.getDate(),23,59,59,999)+d.getTimezoneOffset()*60000+1);document.cookie=\''.$cookieVar.'=h; expires=\'+d.toGMTString()+\'; path=/\';document.getElementById(\'bn0902\').style.display=\'none\'}</script><div style="position: fixed; top: 0; right: 50%; margin-right: -507px; width: '.$width.'px; height: '.$height.'px; z-index: 9999;'.$css.'" id="bn0902" onclick="closeFlashLayer()"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="'.$width.'" height="'.$height.'" id="mymoviename""><param name="movie" value="' . $w['layerPath'] . '?clickTAG='.$bgURL.'" /><param name="quality" value="high" /><param name="allowscriptaccess" value="always"><param name="quality" value="high" /><param name="wmode" value="transparent" /><param name="bgcolor" value="transparent" /><embed src="' . $w['layerPath'] . '?clickTAG='.$bgURL.'" wmode="transparent" quality="high" bgcolor="transparent" width="'.$width.'" height="'.$height.'" name="mymoviename" allowscriptaccess="always" align="" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></div>' . $res;
+			}
+		}
+		return TRUE;
+	}
+	return FALSE;
+
+}
+
 }
