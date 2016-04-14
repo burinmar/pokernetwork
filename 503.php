2f72cf3be76ecc5c19a147f4a63853ae2ca30978
Nikita Udovenko <nikita.udovenko@ibusmedia.com>
2016-04-14 17:48:04 +0300

    Convert php short tags

--- 503.php
+++ 503.php
@@ -1,4 +1,4 @@
-<?
+<?php
 header('HTTP/1.1 503 Service Temporarily Unavailable',true,503);
 header('Status: 503 Service Temporarily Unavailable');
 require_once("503.htm");
--- moon/modules_adm/users/config.cfg.php
+++ moon/modules_adm/users/config.cfg.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $cfg = array();
 $cfg['users'] = array(
 	//'sys.moduleDir' => 'user/',
